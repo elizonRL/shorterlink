a6e4c6d3445da963d715f32f6b2e90fd132eda21
ElizonRL <elizorodriguez16@gmail.com>
2025-08-08 19:04:29 -0400

    refactor: clean up welcome message and remove unused random string generation

--- src/router/links.router.ts
+++ src/router/links.router.ts
@@ -1,20 +1,12 @@
 import { Router } from "express";
-import * as crypto from "crypto";
-import { nanoid } from "nanoid";
 import { setShortenedUrl } from "../controllers/shorten.js";
-
-const getRandomString = (): string => {
-    return crypto.randomBytes(4).toString('hex');
-}
 
 const linksRouter = Router();
 
 linksRouter.get('/', (_req, res) => {
     res.json(
         {
-            "message": "Hello World!",
-            "randomString": getRandomString(),
-            "nanoid": nanoid(8),
+            message: "Welcome to the URL Shortener API",
         }
     );
 });
