5587fcf5d8acb0bae6ef8cf8724098ff2a5c899a
ElizonRL <elizorodriguez16@gmail.com>
2025-08-11 17:46:03 -0400

    database compliete funcionality

--- src/controllers/shorten.ts
+++ src/controllers/shorten.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import type {  Links, shortUrlCode } from "../interface.ts";
+import LinksModels from "../models/linsk.models.js";
 
 let links: Links[] = [];
 
@@ -21,6 +22,11 @@ export const setShortenedUrl = (req:Request, res:Response) => {
             shortUrlCode: shortUrlCode,
         };
         links.push(link);
+        const newLink = new LinksModels({
+            originalUrl: originalUrl,
+            shortUrl: shortUrlCode,
+        });
+        newLink.save();
         return res.status(201).json(link);
     }catch(err){
         return res.status(500).json({ error: "Internal Server Error" });
@@ -36,18 +42,25 @@ export const getShortenedUrl = (req: Request, res: Response) => {
         return res.status(400).json({ error: "Short URL code is required" });
     }
     
-    const link = links.find(l => l.shortUrlCode === shortUrlCode);
-    if (!link) {
-        return res.status(404).json({ error: "Short URL not found" });
-    }
-
-    return res.status(308).redirect(link.originalUrl);
+    LinksModels.findOne({shortUrl: shortUrlCode}).then((link)=>{
+        if(!link){
+            return res.status(404).json({ error: "Short URL not found in DB" });
+        }
+        return res.status(308).redirect(link.originalUrl);
+    }).catch((err)=>{
+        return res.status(500).json({ error: "Internal Server Error" , details: err});
+    })
     // Logic to retrieve the original URL based on the shortUrlCode
     // This is a placeholder as the actual retrieval logic would depend on your data storage solution.
     
 }
 export const getAll = (_req: Request, res: Response) => {
-    res.json(
-        links
-    );
+    LinksModels.find({}).then((links)=>{
+        return res.json(
+            links
+        );
+    }).catch((err)=>{
+        return res.status(500).json({ error: "Internal Server Error" , details: err});
+    })
+    return
 }
