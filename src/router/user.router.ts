c8048927a946e9648c59446734e372542bf2e1f4
Elizon Rodrigues Liriano <118481749+elizonRL@users.noreply.github.com>
2025-08-26 10:44:05 -0400

    remove unused user routes from user router

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createUser, getUser, getAllUsers, loginUser } from "../controllers/user.js";
+import { createUser, loginUser } from "../controllers/user.js";
 
 const userRouter = Router();
 
 // Define user-related routes here
-userRouter.get('/:id', getUser);
-userRouter.get('/', getAllUsers);
+
 
 userRouter.post('/', createUser);
 userRouter.post('/login', loginUser); // Assuming you have a login controller
