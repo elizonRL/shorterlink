794e1658d38f7cfa5e291f766f95ed5e4b42e100
ElizonRL <elizorodriguez16@gmail.com>
2025-08-28 15:47:42 -0400

    refactor: remove commented alternative response in unknownEndpoint middleware

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -14,9 +14,6 @@ export const logger = (req: Request, _res: Response, next: NextFunction) => {
 // Middleware to handle unknown endpoints
 export const unknownEndpoint = (_req: Request, res: Response) => {
     res.status(404).send({ error: 'unknown endpoint' });
-    /* Alternatively, you can send a simple text response:
-    res.status(404).send('unknown endpoint');
-    */
 }
 
 export const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
